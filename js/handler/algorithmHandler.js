968322922da92bd7d7adb8cac709b67f8e5f19fc
a-pompom <aoi.matsuda.3x@gmail.com>
2019-09-11 09:01:18 +0900

    コードリファクタ

---
+++ js/handler/algorithmHandler.js
@@ -0,0 +1,92 @@
+import Dom from '../util/domManipulator.js';
+import Validator from '../util/validator.js'
+import EventHandler from './eventHandler.js';
+
+import LinearSearch from '../algorithm/linearSearch.js';
+import SelectionSort from '../algorithm/selectionSort.js';
+
+/**
+*
+* 探索・ソートを操作するためのハンドラ
+* 探索・ソートアルゴリズムを動的に変更可能とし、かつ、
+* 各アルゴリズムを呼び出し可能とする
+*/
+export default class AlgorithmHandler extends EventHandler{
+	
+	constructor(listContainer, defaultEvent) {
+		
+		super(listContainer, defaultEvent);
+		
+		// 呼び出し可能なアルゴリズム
+		this.SearchAlgorithmList = {LinearSearch};
+		this.SortAlgorithmList = {SelectionSort};
+		
+		// ソート・探索イベント発火時に実行されるアルゴリズム
+		this.currentSearch = null;
+		this.currentSort = null;
+		
+		// デフォルトのアルゴリズムを設定
+		this.overrideSortAlgorithm('SelectionSort');
+		this.overrideSearchAlgorithm('LinearSearch');
+		
+		let funcList = [this.searchButtonClick, this.sortButtonClick];
+		super.registerEvent(['searchButton', 'sortButton'], 'click', funcList);
+	}
+	
+	// ソート・探索イベント発火時に実行されるアルゴリズムを動的に変更
+	overrideSortAlgorithm(sortAlgorithm) {
+		this.currentSort = sortAlgorithm;
+	}
+	overrideSearchAlgorithm(searchAlgorithm) {
+		this.currentSearch = searchAlgorithm;
+	}
+	
+	// 探索
+	searchButtonClick() {
+		this.defaultEvent.clear();
+		this.searchItem();
+	}
+	// ソート
+	sortButtonClick() {
+		this.defaultEvent.clear();
+		
+		this.sortItem();
+	}
+	
+	 /**
+	 * リストを探索する
+	 */
+	searchItem() {
+		// 探索要素
+		let target = document.getElementById('searchText').value;
+		// バリデーション
+		if (!Validator.validateNumericInput(target)) {
+			return;
+		}
+		this.listContainer.target = parseInt(target);
+		
+		// 探索アルゴリズムを決定
+		const SearchAlgorithm = new this.SearchAlgorithmList[this.currentSearch](this.listContainer);
+
+		// 探索
+		SearchAlgorithm.search();
+	}
+	
+	sortItem() {
+		// ソートアルゴリズムを決定
+		const SortAlgorithm = new this.SortAlgorithmList[this.currentSort](this.listContainer);
+		
+		let sortedItemList = SortAlgorithm.sort();
+		
+		// ソート後には並べ替えのため再描画が必要となるので、既存要素は削除
+		this.defaultEvent.deleteElement();
+		
+		// 再描画
+		sortedItemList.forEach((element) => {
+			this.listContainer.itemList.push(element);
+			Dom.appendItem(this.listContainer.itemList);
+		});
+		
+	}
+	
+}
---
+++ js/handler/basicEventHandler.js
@@ -0,0 +1,69 @@
+import EventHandler from './eventHandler.js';
+import Dom from '../util/domManipulator.js';
+
+/**
+*
+* 他のハンドラからも参照されるような汎用的なイベントを管理する
+*/
+export default class BasicEventHandler extends EventHandler{
+	
+	// 削除・クリアを登録
+	constructor(listContainer, dom) {
+		super(listContainer, dom);
+		
+		let funcList = [this.deleteButtonClick, this.clearButtonClick];
+		super.registerEvent(['deleteButton', 'clearButton'], 'click', funcList);
+		
+	}
+	
+	// 削除
+	deleteButtonClick() {
+		this.clear();
+		this.deleteElement();
+	}
+	// クリア
+	clearButtonClick() {
+		this.clear();
+	}
+	
+	 /**
+	 * 再探索ができるようボタンクリックで毎回探索結果をクリア
+	 */
+	clear() {
+		this.clearErrorMessage();
+		this.clearStyleClass();
+		
+		this.listContainer.matched = false;
+	}
+
+	/**
+	 * エラーメッセージをクリア
+	 */
+	clearErrorMessage() {
+		Dom.clearMessage('errorMessageText');
+	}
+
+	/**
+	 * 要素に割り当てられたスタイルを初期化
+	 */
+	clearStyleClass() {
+
+		this.listContainer.itemList.forEach((element, index) => {
+			Dom.removeStyleClass('item-' + index, 'targetItem');
+			Dom.removeStyleClass('item-' + index, 'searchedItem');
+		});
+	}
+
+	/**
+	 * 探索要素を全削除する
+	 * 探索要素の配列・探索要素のDOMを初期化する
+	 */
+	deleteElement() {
+		this.listContainer.itemList.forEach((element, index) => {
+			document.getElementById('item-' + index).remove();
+		});
+		this.listContainer.itemList = [];
+	}
+	
+	
+}
